30d132e91d013f042a5350aa47b88b611d5f066d
larry <larrylv@juxinli.com>
2015-05-29 14:21:49 +0800

    添加LRU MAP

---
+++ src/test/java/com/infogen/LRU.java
@@ -0,0 +1,21 @@
+package com.infogen;
+
+import com.infogen.cache.LRULinkedHashMap;
+
+/**
+ * @author larry/larrylv@outlook.com/创建时间 2015年5月29日 下午2:18:27
+ * @since 1.0
+ * @version 1.0
+ */
+public class LRU {
+	public static void main(String[] args) {
+		LRULinkedHashMap<Integer, String> map = new LRULinkedHashMap<>(10);
+		for (int i = 0; i < 100; i++) {
+			map.put(i, i + "");
+		}
+
+		for (String string : map.values()) {
+			System.out.println(string);
+		}
+	}
+}
