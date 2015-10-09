cd7b165ce077fdadbcd35390f46a417183b48bb5
larry <larrylv@juxinli.com>
2015-10-09 12:36:23 +0800

    更新版本

--- src/test/java/com/infogen/tools/Tool_CoreTest.java
+++ src/test/java/com/infogen/tools/Tool_CoreTest.java
@@ -16,45 +16,45 @@ import com.infogen.core.tools.Tool_Core;
 
 public class Tool_CoreTest {
 
-	@Test(groups = { "tools","all" })
+	@Test(groups = { "tools", "all" })
 	public void stacktrace() {
 		List<String> list = new ArrayList<String>();
-		try{
+		try {
 			list.get(1);
-		}catch(IndexOutOfBoundsException e){
+		} catch (IndexOutOfBoundsException e) {
 			String str = Tool_Core.stacktrace(e);
 			Assert.assertNotNull(str);
 		}
 	}
 
-	@Test(groups = { "tools","all" })
-	public void prepare_files(){
-		// TODO  好像不能创建缺失的文件夹		
+	@Test(groups = { "tools", "all" })
+	public void prepare_files() {
+		// TODO 好像不能创建缺失的文件夹
 		Path path = Paths.get("a.b.c.t");
 		Path path2 = Paths.get("a.m.n.t");
-		Tool_Core.prepare_files(path,path2);
+		Tool_Core.prepare_files(path, path2);
 		boolean path_exists = path.toFile().exists();
 		boolean path2_exists = path2.toFile().exists();
-		Assert.assertEquals(path_exists, true); 
-		Assert.assertEquals(path2_exists, true); 
+		Assert.assertEquals(path_exists, true);
+		Assert.assertEquals(path2_exists, true);
 		path.toFile().deleteOnExit();
 		path2.toFile().deleteOnExit();
 	}
-	
-	@Test(groups = { "tools","all" })
-	public void load_file() throws IOException{
+
+	@Test(groups = { "tools", "all" })
+	public void load_file() throws IOException {
 		Path path = Paths.get("a.b.c.t");
 		Tool_Core.prepare_files(path);
 		List<String> lines = new ArrayList<String>();
 		lines.add("line1:something write.");
 		lines.add("line2:something write else.");
-		Files.write(path, lines, StandardOpenOption.APPEND);		
+		Files.write(path, lines, StandardOpenOption.APPEND);
 		String s = Tool_Core.load_file(path);
 		Assert.assertEquals(s, "line1:something write.line2:something write else.");
 		path.toFile().deleteOnExit();
 	}
-	
-	@Test(groups = { "tools","all" })
+
+	@Test(groups = { "tools", "all" })
 	public void MD5() throws NoSuchAlgorithmException {
 		String password = "a";
 		String HexString = "1";
@@ -63,19 +63,19 @@ public class Tool_CoreTest {
 		Assert.assertNotEquals(result, password);
 	}
 
-	@Test(groups = { "tools","all" })
+	@Test(groups = { "tools", "all" })
 	public void getHostName() {
 		String host = Tool_Core.getHostName();
 		Assert.assertNotNull(host);
 	}
 
-	@Test(groups = { "tools","all" })
+	@Test(groups = { "tools", "all" })
 	public void getLocalIP() {
-		String localIP = Tool_Core.getLocalIP();
+		String localIP = Tool_Core.getLocalIP("eth,wan");
 		Assert.assertNotNull(localIP);
 	}
 
-	@Test(groups = { "tools","all" })
+	@Test(groups = { "tools", "all" })
 	@Deprecated
 	public void ip_to_long() {
 		Tool_Core tool = new Tool_Core();
@@ -83,7 +83,7 @@ public class Tool_CoreTest {
 		Assert.assertEquals(ipLong, new Long("3232261266"));
 	}
 
-	@Test(groups = { "tools","all" })
+	@Test(groups = { "tools", "all" })
 	public void trim() {
 		String str = "  a  b  ";
 		String resultStr = Tool_Core.trim(str);
