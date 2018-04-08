95918b37bf2f0428307eb6827dec051f59ec8e8b
larry <larrylv@newcreds.com>
2018-04-08 17:12:58 +0800

    删除启动监听

--- src/main/java/com/infogen/http/InfoGen_Listener.java
+++ src/main/java/com/infogen/http/InfoGen_Listener.java
@@ -10,6 +10,8 @@ import javax.servlet.annotation.WebListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.infogen.InfoGen;
+
 /**
  * @author larry
  * @version 创建时间 2018年3月26日 下午5:37:16
@@ -19,6 +21,7 @@ public class InfoGen_Listener implements ServletContextListener {
 	private static final Logger LOGGER = LogManager.getLogger(InfoGen_Listener.class.getName());
 
 	public void contextInitialized(ServletContextEvent event) {
+		InfoGen.aop();
 		LOGGER.info("servlet context created - JAR SOA");
 	}
 
