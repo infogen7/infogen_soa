05b35f5c41cd115ab59494287c737e65ffe72064
larry <larrylv@newcreds.com>
2018-05-19 00:27:44 +0800

    update

--- src/main/java/com/infogen/sql/SqlValidation.java
+++ src/main/java/com/infogen/sql/SqlValidation.java
@@ -11,7 +11,11 @@ import java.util.regex.Pattern;
 public class SqlValidation {
 	// 判断是否存在sql注入
 	public static Boolean is_sql_safety(String string) {
-		return Regular(string, "^[_0-9a-zA-Z\\u4e00-\\u9fa5]{1,32}$");
+		if (null == string || string.trim().length() <= 0) {
+			return false;
+		}
+		//- 多用于 CODE 等业务，但是--可能被用来做注入
+		return Pattern.compile("^[-_0-9a-zA-Z\\u4e00-\\u9fa5]{1,32}$").matcher(string).matches() && string.indexOf("--") == -1;
 	}
 
 	public static String[] is_sql_safety(String[] item_array) {
@@ -19,26 +23,11 @@ public class SqlValidation {
 		for (int i = 0; i < item_array.length; i++) {
 			String string = item_array[i];
 
-			if (Regular(string, "^[_0-9a-zA-Z\\u4e00-\\u9fa5]{1,32}$")) {
+			if (is_sql_safety(string)) {
 				return_list.add(string);
 			}
 		}
 		return return_list.toArray(new String[] {});
 	}
 
-	/**
-	 * 匹配是否符合正则表达式pattern 匹配返回true
-	 * 
-	 * @param str
-	 *            匹配的字符串
-	 * @param pattern
-	 *            匹配模式
-	 * @return Boolean
-	 */
-	private static Boolean Regular(String string, String pattern) {
-		if (null == string || string.trim().length() <= 0) {
-			return false;
-		}
-		return Pattern.compile(pattern).matcher(string).matches();
-	}
 }
